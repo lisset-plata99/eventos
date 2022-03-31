893b0daae39d01d4cdaf29baf088e7f9fbf03d53
lisset-plata99 <lisset_1320114016@uptecamac.edu.mx>
2022-03-31 11:15:25 -0600

    Update noticias.blade.php

--- resources/views/noticias.blade.php
+++ resources/views/noticias.blade.php
@@ -73,56 +73,37 @@
 <!--/ bradcam_area  -->
 
 <!-- Start Align Area -->
+
+<?php
+                        $url = "https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=QcIfqTH4BfZ8AhE9c3PqYeNe5evlSlaf";
+                        $data = json_decode(file_get_contents($url));
+						// echo $data-> results[0]->title;
+						// echo $data-> results[0]->abstract;
+						// echo $data-> results[0]->multimedia[0]->url;
+                        ?>
+
 	<div class="whole-wrap">
 		<div class="container box_1170">
 			<div class="section-top-border">
-				<h3 class="mb-30">Left Aligned</h3>
+				<h3 class="mb-30"><?php echo $data-> results[0]->title;?></h3>
 				<div class="row">
 					<div class="col-md-3">
-						<img src="../img/elements/d.jpg" alt="" class="img-fluid">
+						<img src="<?php echo  $data-> results[0]->multimedia[0]->url;?>" alt="" class="img-fluid">
 					</div>
+					
 					<div class="col-md-9 mt-sm-20">
-						<p>Recently, the US Federal government banned online casinos from operating in America by making
-							it illegal to
-							transfer money to them through any US bank or payment system. As a result of this law, most
-							of the popular
-							online casino networks such as Party Gaming and PlayTech left the United States. Overnight,
-							online casino
-							players found themselves being chased by the Federal government. But, after a fortnight, the
-							online casino
-							industry came up with a solution and new online casinos started taking root. These began to
-							operate under a
-							different business umbrella, and by doing that, rendered the transfer of money to and from
-							them legal. A major
-							part of this was enlisting electronic banking systems that would accept this new
-							clarification and start doing
-							business with me. Listed in this article are the electronic banking systems that accept
-							players from the United
-							States that wish to play in online casinos.</p>
+						<p><?php echo $data-> results[0]->abstract;?></p>
 					</div>
 				</div>
 			</div>
 			<div class="section-top-border text-right">
-				<h3 class="mb-30">Right Aligned</h3>
+				<h3 class="mb-30"><?php echo $data-> results[1]->title;?></h3>
 				<div class="row">
 					<div class="col-md-9">
-						<p class="text-right">Over time, even the most sophisticated, memory packed computer can begin
-							to run slow if we
-							don’t do something to prevent it. The reason why has less to do with how computers are made
-							and how they age and
-							more to do with the way we use them. You see, all of the daily tasks that we do on our PC
-							from running programs
-							to downloading and deleting software can make our computer sluggish. To keep this from
-							happening, you need to
-							understand the reasons why your PC is getting slower and do something to keep your PC
-							running at its best. You
-							can do this through regular maintenance and PC performance optimization programs</p>
-						<p class="text-right">Before we discuss all of the things that could be affecting your PC’s
-							performance, let’s
-							talk a little about what symptoms</p>
+						<p class="text-right"><?php echo $data-> results[1]->abstract;?></p>
 					</div>
 					<div class="col-md-3">
-						<img src="../img/elements/d.jpg" alt="" class="img-fluid">
+						<img src="<?php echo  $data-> results[1]->multimedia[0]->url;?>" alt="" class="img-fluid">
 					</div>
 				</div>
 			</div>
